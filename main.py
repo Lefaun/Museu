a87c8f4068da7d7a2b4a904bf0efd97f2c3454cf
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-02-22 18:10:58 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,22 @@ import streamlit as st
 
 import streamlit.components.v1 as components
 
-
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('backg.png')    
 
 st.title("Museu Virtual da Arte Urbana - AMadora")
 
