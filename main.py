69666e7dec4c72ddd73b4ebfd34378e233762fa5
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-02-22 16:07:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ st.title("Museu Virtual da Arte Urbana - AMadora")
 col1, col2, col3 = st.columns(3)
 with col1:
    st.header("Museu - Arte Urbana ")
-   st.image("Museu/1foto.png")
+   st.image("1foto.png")
 
 with col2:
    st.header("Criação de APP Online")
-   st.image("Museu/2foto.png")
+   st.image("2foto.png")
 
 with col3:
    st.header("Modelação Urbana")
-   st.image("Museu/4foto.png")
+   st.image("4foto.png")
 
 
 video_file = open('/Museu/video.mov', 'rb')
