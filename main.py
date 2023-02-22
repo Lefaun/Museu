3175446d39bdd67d930b71ec9b70dd30b5aafa12
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-02-22 15:52:52 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 
+
 import streamlit as st
 
 import streamlit.components.v1 as components
 st.title("Museu Virtual da Arte Urbana - AMadora")
-components.iframe("https://v3d.net/fbi", width=1024, height=640)
 
+col1, col2, col3 = st.columns(3)
+with col1:
+   st.header("Museu - Arte Urbana ")
+   st.image("1foto.png")
+
+with col2:
+   st.header("Criação de APP Online")
+   st.image("2foto.png")
+
+with col3:
+   st.header("Modelação Urbana")
+   st.image("4foto.png")
+
+
+video_file = open('video.mov', 'rb')
+video_bytes = video_file.read()
+
+st.video(video_bytes)
+
+components.iframe("https://v3d.net/fbi", width=1800, height=600)
 st.markdown(components.iframe, unsafe_allow_html=True)
