c35c6c78e61ea67f9e1dd489dc3d6eb6677b7a26
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-02-22 17:50:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def set_background(png_file):
     ''' % bin_str
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
-set_background('./images/background.png')
+set_background('Screenshot 2023-02-22 at 17.40.37.png')
 
 st.title("Museu Virtual da Arte Urbana - AMadora")
 
