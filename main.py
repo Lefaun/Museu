d0710cb629ff3ce65df006a6c4c20c0255fde568
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-02-22 17:56:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,8 @@
 import streamlit as st
 
 import streamlit.components.v1 as components
-import base64
-import streamlit as st
 
 
-def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-
-def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-
-set_background('Screenshot 2023-02-22 at 17.40.37.png')
 
 st.title("Museu Virtual da Arte Urbana - AMadora")
 
